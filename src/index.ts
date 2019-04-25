58090835ca9b5f9c4a1bcf7defe878a237ddd0ac
Chris Weiss <bitblit@gmail.com>
2019-04-24 21:41:36 -0700

    Checkpoint - simple test works with typescript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-/**
- * Just a placeholder at this point
- **/
+import {SimpleTest} from './parrot/simple-test';
+import {Logger} from '@bitblit/ratchet/dist/common/logger';
+
+const inst: SimpleTest = new SimpleTest();
+
+inst.run().then(()=>{Logger.info('Done')});
--- src/parrot/simple-test.ts
+++ src/parrot/simple-test.ts
@@ -17,21 +17,36 @@ export class SimpleTest {
     private takeoff = this.parser.getCommand('minidrone', 'Piloting', 'TakeOff');
     private landing = this.parser.getCommand('minidrone', 'Piloting', 'Landing');
     private backFlip = this.parser.getCommand('minidrone', 'Animations', 'Flip', {direction: 'back'});
+    private leftFlip = this.parser.getCommand('minidrone', 'Animations', 'Flip', {direction: 'left'});
+
+    //private rotateAxis = this.parser.getCommand('minidrone', 'Animations', 'horizontal_panorama', {direction: 'left'});
 
     constructor(){}
 
-    public async run(): Promise<void> {
-        this.drone.on('connected',  async () => {
-            await this.runCommand(this.takeoff, 2000);
-            await this.runCommand(this.backFlip, 4000);
-            await this.runCommand(this.landing, 5000);
-            process.exit;
+    public run(): Promise<boolean> {
+
+        return new Promise<boolean>((res,rej)=>{
+            Logger.info('Running...');
+            this.drone.on('connected',  async () => {
+                Logger.info('Connected');
+                await this.runRoutine();
+                Logger.info('Done, stopping');
+                res(true);
+            });
         });
+
+    }
+
+    public async runRoutine(): Promise<void> {
+        await this.runCommand(this.takeoff, 2000);
+        await this.runCommand(this.backFlip, 4000);
+        await this.runCommand(this.leftFlip, 4000);
+        await this.runCommand(this.landing, 5000);
     }
 
     public async runCommand(cmd: any, delay:number=1000): Promise<void>{
         Logger.info('Running %j', cmd);
-        this.drone.runCommand(cmd);
+        await this.drone.runCommand(cmd);
         await PromiseRatchet.createTimeoutPromise('Wait',delay, true, false);
     }
 
